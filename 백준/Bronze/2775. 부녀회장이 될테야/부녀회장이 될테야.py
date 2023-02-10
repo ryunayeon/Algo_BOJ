2e2a1f416292b4ec9b1f9e2298c26f07fdfd892e
Nayeon Ryu <122508466+ryunayeon@users.noreply.github.com>
2023-02-10 17:46:06 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 72 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -0,0 +1,9 @@
+T = int(input())
+for _ in range(1, T+1):
+    k = int(input())
+    n = int(input())
+    people = [i for i in range(1, n+1)]
+    for _ in range(k):
+        for j in range(1, n):
+            people[j] += people[j-1]
+    print(people[-1])
